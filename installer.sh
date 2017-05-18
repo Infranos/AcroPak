d545d7f3158fb1e293e836b7b9c9214f77a2df64
Infranos <Melancholy94@gmail.com>
2017-05-18 04:58:36 -0700

    Update installer.sh
    Wanted to accommodate users who installed from GitHub

--- installer.sh
+++ installer.sh
@@ -7,22 +7,26 @@ CHECKER=/tmp/check.txt
 locate AcroPak | grep AcroPak$ > $CHECKER
 
 if [ ! -s $CHECKER ]; then
-	echo Error, installation folder not found.
-	echo Please place the AcroPak folder onto your system.
+	locate AcroPak | grep AcroPak-master$ > $CHECKER
+	if [ ! -s $CHECKER ]; then
+		echo Error, installation folder not found.
+		echo Please place the AcroPak folder onto your system.
+		exit -1
+	fi
+fi
+
+FOLDER=`cat $CHECKER`
+
+if [ ! -s $FOLDER/acro ]; then
+	echo Error, acro file not found.
+	echo Consider redownloading either the acro file or its respective folder.
 	exit -1
 else
-	FOLDER=`cat $CHECKER`
-	if [ ! -s $FOLDER/acro ]; then
-		echo Error, acro file not found.
-		echo Consider redownloading either the acro file or its respective folder.
-		exit -1
-	else
-		chmod +x $FOLDER/acro
-		sudo cp $FOLDER/acro /usr/bin/acro
-		if [ -s /usr/bin/nym ]; then
-                        sudo rm /usr/bin/nym
-                fi
-		sudo ln /usr/bin/acro /usr/bin/nym
-		echo Installation complete!
-	fi
+	chmod +x $FOLDER/acro
+	sudo cp $FOLDER/acro /usr/bin/acro
+	if [ -s /usr/bin/nym ]; then
+		sudo rm /usr/bin/nym
+        fi
+	sudo ln /usr/bin/acro /usr/bin/nym
+	echo Installation complete!
 fi
